ee9a6b06a148e1f2a12caa07b7112b878286219e
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2022-08-30 19:43:13 -0300

    criando End-point para salvar

--- src/main/java/br/com/springboot/treinamento_spring/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/treinamento_spring/controllers/GreetingsController.java
@@ -1,13 +1,21 @@
 package br.com.springboot.treinamento_spring.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.treinamento_spring.model.Usuario;
 import br.com.springboot.treinamento_spring.repository.UsuarioRepository;
 
 /**
@@ -32,5 +40,24 @@ public class GreetingsController {
         return "Curso APi " + name + "!";
     }
     
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody /*Retorna os dados para o corpo da resposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	
+    	List<Usuario> usuarios =  usuarioRepository.findAll(); /*execulta a consulta no banco de dados*/
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);  /*Retorna a lista em json*/
+    }
+    
+    @PostMapping(value = "salvar") /*mapeia a url*/
+    @ResponseBody /* descrição da resposta*/
+    public ResponseEntity<Usuario> salvar (@RequestBody Usuario usuario){ /*recebe os dados para salvar*/
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    	
+    }
    
 }
